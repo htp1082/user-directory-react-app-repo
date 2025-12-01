b5499ac805c8096439337b6ddf6b33c50288b318
Hla Thue Prue Marma <hlathuepruemarma@Hlas-MacBook-Air.local>
2025-12-01 10:20:37 +0300

    Added to allcountry Fech

--- country-react-project/src/App.jsx
+++ country-react-project/src/App.jsx
@@ -1,8 +1,14 @@
+import { Suspense } from "react";
 import "./App.css";
+import Countrys from "./component/Countrys";
 
+const allCountrysFech = fetch("https://openapi.programming-hero.com/api/all")
+.then(res => res.json());
 function App() {
   return <>
-   <h1>Country-react-project</h1>
+   <Suspense fallback={<p>All data is loading...</p>}>
+     <Countrys allCountrysFech={allCountrysFech}></Countrys>
+   </Suspense>
   </>;
 }
 
--- country-react-project/src/component/Countrys.jsx
+++ country-react-project/src/component/Countrys.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { use } from 'react';
+import Country from '../country/ country';
 
-const Countrys = () => {
+const Countrys = ({allCountrysFech}) => {
+    const allCountry = use(allCountrysFech); 
+    const countries = allCountry.countries;
+    console.log(countries)
+    
+    console.log(allCountry)
     return (
         <div>
+            
+            <h2>All Countrys : {countries.length}</h2>
+
             
         </div>
     );
